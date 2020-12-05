1240b1999a7f5ddf7592fb833f3d463790aedc3f
Bennett Helm <bennett.helm@fandm.edu>
2020-12-05 13:35:08 -0500

    Updates

--- fixYAML.lua
+++ fixYAML.lua
@@ -23,9 +23,15 @@ function fixYAMLHeader(meta)
     end
     if meta.geometry and meta.geometry[1].c == 'ipad' then
         if meta.book then
-            meta.geometry[1].c = 'paperwidth=176mm,paperheight=234mm,outer=22mm,top=2.5pc,bottom=3pc,headsep=1pc,includehead,includefoot,centering,inner=22mm,marginparwidth=17mm'
+            -- meta.geometry[1].c = 'paperwidth=176mm,paperheight=234mm,outer=22mm,top=2.5pc,bottom=3pc,headsep=1pc,includehead,includefoot,centering,inner=22mm,marginparwidth=17mm'
+            meta.geometry[1].c = 'paperwidth=190.5mm,paperheight=271.88mm,outer=22mm,top=2.5pc,bottom=3pc,headsep=1pc,includehead,includefoot,centering,inner=22mm,marginparwidth=17mm'
         else
-            meta.geometry[1].c = 'paperwidth=176mm,paperheight=234mm,width=360.0pt,height=541.40024pt,headsep=1pc,centering'
+            -- meta.geometry[1].c = 'paperwidth=176mm,paperheight=234mm,width=360.0pt,height=541.40024pt,headsep=1pc,centering'
+            -- iPad Pro 11" has screen dimensions 160.2mm x 228.9mm. This is
+            -- 8.24% wider and 16.19% taller than iPad Air 2
+            -- 360 pt = 126.52 mm ... so should add 50mm to width to get paperwidth
+            -- 541.4pt = 190.28 mm ... so should add 44mm to height to get paperheight
+            meta.geometry[1].c = 'paperwidth=190.5mm,paperheight=271.88mm,width=136.95mm,height=221.09mm,headsep=1pc,centering'
         end
     end
     if meta.bibliography then  -- Get complete path to bibliography files
